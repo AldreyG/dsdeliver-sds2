82bb6f9dc49065f976dc90fab57a05746458b2ef
Desenvolvimento <aldrey.georgetti@gmail.com>
2021-01-07 19:22:08 -0300

    Domain model

--- backend/src/main/java/com/dsmaratona/dsdeliver/entities/Product.java
+++ backend/src/main/java/com/dsmaratona/dsdeliver/entities/Product.java
@@ -2,10 +2,22 @@ package com.dsmaratona.dsdeliver.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_product")//nome da tabela no bd
 public class Product implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;//versao
 	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)// identifica atraves do id
 	private Long id;
 	private String name;
 	private Double price;
